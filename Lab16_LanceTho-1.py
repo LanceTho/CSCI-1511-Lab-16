1f0512f9875a5585ec682ac843595107d3e78cb1
Lance Thongsavanh <lancer8106@gmail.com>
2026-05-05 13:11:49 -0400

    Reads the data from the csv file and plots the unemployment rates on a graph

--- Lab16_LanceTho-1.py
+++ Lab16_LanceTho-1.py
@@ -4,7 +4,34 @@ Lance Thongsavanh
 Write a program that reads the OHRU.csv file, which contains Ohio's unemployment rate since 1976. You will parse this data and create a time-series line plot using matplotlib.
 Date.
 """
+from pathlib import Path
+import csv
+import matplotlib.pyplot as plt
+import datetime
 
+path = Path("OHUR.csv")
+lines = path.read_text().splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+
+# for index, col_title in enumerate(header_row):
+#     print(f"{index} {col_title}, ", end=" ")
+# print()
+
+dates: list = []
+unemp_rates: list[float] = []
+
+for row in reader:
+    date = row[0]
+    rate = float(row[1])
+    dates.append(date)
+    unemp_rates.append(rate)
+
+figure, graph = plt.subplots()
+
+graph.plot(unemp_rates)
+plt.show()
 """
 Requirements:
 You must import matplotlib.pyplot as plt, csv, and datetime.
